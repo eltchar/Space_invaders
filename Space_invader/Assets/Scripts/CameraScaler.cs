770031463760d1a708d72112ed3c870f4bbf024e
eltchar <eltchar@gmail.com>
2020-11-05 23:13:01 +0100

    First commit, main menu and screen transition

---
+++ Space_invader/Assets/Scripts/CameraScaler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScaler : MonoBehaviour
+{
+    public SpriteRenderer border;
+
+    // Use this for initialization
+    void Start()
+    {
+        float currentRatio = (float)Screen.width / (float)Screen.height;
+        float targetRatio = (border.bounds.size.x / border.bounds.size.y)*1.2f;
+
+        if (currentRatio >= targetRatio)
+        {
+            Camera.main.orthographicSize = border.bounds.size.y / 2;
+        }
+        else
+        {
+            float differenceInSize = targetRatio / currentRatio;
+            Camera.main.orthographicSize = border.bounds.size.y / 2 * differenceInSize;
+        }
+    }
+}
---
+++ Space_invader/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagerScript : MonoBehaviour
+{
+
+    public static GameManagerScript instance { get; private set; }
+
+    private void Awake()
+    {
+        //Setting up singleton pattern
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void MoveToLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void MoveToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
